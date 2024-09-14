a3545d82c5cb4f42bed961eb5d8ffa3d1f6e6f99
Neil Ross <neil.ross@mixcloud.com>
2024-09-14 08:24:51 +0100

    add invite page

--- app/routes/invite.$contactId.tsx
+++ app/routes/invite.$contactId.tsx
@@ -49,6 +49,7 @@ export default function Contact() {
 
   return (
     <div id="contact" className="container mx-auto px-12">
+      <img src="/letters.svg" alt="letters" className="initials" />
       <div className="pt-10 text-center">
         <h5>Dear</h5>
         <h1>{contact.guest ? <>{contact.guest}</> : <i>No Name</i>}</h1>
