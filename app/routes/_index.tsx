20df1c34def7e08161eb7361f5e57f61c3b421ba
Neil Ross <neil.ross@mixcloud.com>
2024-09-28 22:53:51 +0100

    Add landingpage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,35 +7,37 @@ export const meta: MetaFunction = () => {
       name: "description",
       content: "Welcome to Remix on Cloudflare!",
     },
+    { robots: "noindex, nofollow" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix on Cloudflare</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <img
+        src="hero.jpg"
+        alt="Jo & Neil standing on top of Ben Nevis looking into each other's eyes"
+        className="w-full h-96 object-cover"
+      />
+      <div className="mx-auto max-w-7xl px-4 py-24 sm:px-6 sm:py-32 lg:px-8">
+        <div className="mx-auto max-w-2xl">
+          <h1 className="text-center leading-tight">
+            Jo &amp; Neil
+            <br /> are <br /> Getting Hitched!
+          </h1>
+
+          <div className="text-center m-10">
+            <p>
+              If you are reading this you probably know us, please scan your
+              invite for more info.{" "}
+            </p>
+            <p className="pt-6 bold">
+              Lost your invite?{" "}
+              <a className="italic" href="mailto:neilross.wkdesign@gmail.com">Contact Neil</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
